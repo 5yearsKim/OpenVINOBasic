093eb35d553ef93230385807092a6af56e52488f
onion.kim <onion.kim@intel.com>
2025-06-23 03:57:41 +0900

    Sample project (hello detector) added
    Signed-off-by: onion.kim <onion.kim@intel.com>

---
+++ hello_detection/main.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
+
+from hello_detector import HelloDetector
+from notebook_utils import download_file
+
+base_model_dir = Path("./model").expanduser()
+
+model_name = "horizontal-text-detection-0001"
+model_xml_name = f"{model_name}.xml"
+model_bin_name = f"{model_name}.bin"
+
+model_xml_path = base_model_dir / model_xml_name
+
+
+detector = HelloDetector(model_path=model_xml_path, device="CPU")
+
+
+image_filename = Path("data/intel_rnb.jpg")
+
+if not image_filename.exists():
+    image_filename = download_file(
+        "https://storage.openvinotoolkit.org/repositories/openvino_notebooks/data/data/image/intel_rnb.jpg",
+        directory="data",
+    )
+
+# Text detection models expect an image in BGR format.
+image = cv2.imread(str(image_filename))
+
+boxes, resized_image = detector.detect(image)
+
+
+img = detector.convert_result_to_image(image, resized_image, boxes, conf_labels=False)
+
+
+plt.figure(figsize=(10, 6))
+plt.imshow(img)
+plt.axis("off")
+
+plt.show()
